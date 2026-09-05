84d8022804bf699c872d55146c606a3f74a622da
PradipKafleDev <pradip.kafle@zepben.com>
2026-09-05 13:37:47 +1000

    Add BlurTube extension icons

--- manifest.config.ts
+++ manifest.config.ts
@@ -4,16 +4,28 @@ export default defineManifest({
   manifest_version: 3,
 
   name: "BlurTube",
-
   version: "1.0.0",
 
-  description: "Blur YouTube thumbnails for more private browsing.",
+  description:
+      "A lightweight Chrome extension for blurring YouTube thumbnails, titles, and channel information.",
 
   permissions: ["storage"],
+
+  icons: {
+    "16": "icons/icon16.png",
+    "32": "icons/icon32.png",
+    "48": "icons/icon48.png",
+    "128": "icons/icon128.png",
+  },
 
   action: {
     default_popup: "src/popup/index.html",
     default_title: "BlurTube",
+    default_icon: {
+      "16": "icons/icon16.png",
+      "32": "icons/icon32.png",
+      "48": "icons/icon48.png",
+    },
   },
 
   content_scripts: [
